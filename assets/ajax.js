e82557028fefede600e4aafd068308ea88a39349
kakin <kaisboulakhlas9@gmail.com>
2021-06-23 18:54:49 +0200

    Add modal generic + ajax

--- assets/ajax.js
+++ assets/ajax.js
@@ -1,180 +1,24 @@
 import $ from "jquery";
 
-export const ajaxModalHome = () => {
-    $('#new_range').submit(function (e) {
-        const newUrl = "/home/range/add";
-        e.preventDefault();
+export const AjaxModal = () => {
+    const modalGeneric = $('#modalGeneric')
+    modalGeneric.on('shown.bs.modal', function (e) {
+        const url = $(e.relatedTarget).data("url")
+        $.get(url, function(res){
+            $('#modal-generic-content').html($(res).find('.modal-content'))
+        })
+    })
 
-        $.ajax({
-            type: $(this).attr('method'),
-            url: newUrl,
-            data: $(this).serialize(),
-            beforeSend: function(){
-                $('#new_range .modal-body').replaceWith(
-                    `<div class="container p-5 text-center">
-                        <div class="spinner-border text-primary" role="status">
-                            <span class="sr-only">Loading...</span>
-                        </div>
-                    </div>`
-                );
-            },
-            success: function () {
-                $('.spinner-border').css('display','none');
-                $('#addRangeModal').modal('hide');
-                window.location = '/home';
-            },
-            error: function (xhr) {
-                console.log(xhr, "Erreur lors de l'ajout");
-            }
-        });
-    });
-
-    $('#new_workstation').submit(function (e) {
-        const newUrl = "workstation/add";
-        e.preventDefault();
-
-        $.ajax({
-            type: $(this).attr('method'),
-            url: newUrl,
-            data: $(this).serialize(),
-            beforeSend: function(){
-                $('#new_workstation .modal-body').replaceWith(
-                    `<div class="container p-5 text-center">
-                        <div class="spinner-border text-primary" role="status">
-                            <span class="sr-only">Loading...</span>
-                        </div>
-                    </div>`
-                );
-            },
-            success: function () {
-                $('.spinner-border').css('display','none');
-                $('#addWorkStationModal').modal('hide');
-                window.location = '/workstations';
-            },
-            error: function (xhr) {
-                console.log(xhr, "Erreur lors de l'ajout");
-            }
-        });
-    });
-
-    $('#new_range_realisation').submit(function (e) {
-        const newUrl = "/rangeRealisation/add";
-        e.preventDefault();
-
-        $.ajax({
-            type: $(this).attr('method'),
-            url: newUrl,
-            data: $(this).serialize(),
-            beforeSend: function(){
-                $('#new_range_realisation .modal-body').replaceWith(
-                    `<div class="container p-5 text-center">
-                        <div class="spinner-border text-primary" role="status">
-                            <span class="sr-only">Loading...</span>
-                        </div>
-                    </div>`
-                );
-            },
-            success: function () {
-                $('.spinner-border').css('display','none');
-                $('#addRangeRealisationModalLabel').modal('hide');
-                window.location = '/home';
-            },
-            error: function (xhr) {
-                console.log(xhr, "Erreur lors de l'ajout");
-            }
-        });
-    });
-
-    $('form[id^="edit_range"]').submit(function (e) {
-        const newUrl = "home/range/edit/" + $(this).attr('data-whatever');
-        e.preventDefault();
-        $.ajax({
-            type: $(this).attr('method'),
-            url: newUrl,
-            data: $(this).serialize(),
-            beforeSend: function(){
-                $('form[id^="edit_range"] .modal-body').replaceWith(
-                    `<div class="container p-5 text-center">
-                        <div class="spinner-border text-primary" role="status">
-                            <span class="sr-only">Loading...</span>
-                        </div>
-                    </div>`
-                );
-            },
-            success: function () {
-                $('.spinner-border').css('display','none');
-                $('div[aria-labelledby="editRangeModalLabel"]').modal('hide');
-                window.location="/home";
-            },
-            error: function (xhr) {
-                console.log(xhr, "Erreur lors de la modification");
-            }
-        });
-    });
-    $('form[id^="edit_workstation"]').submit(function (e) {
-        const newUrl = "workstation/edit/" + $(this).attr('data-whatever');
-        e.preventDefault();
-        $.ajax({
-            type: $(this).attr('method'),
-            url: newUrl,
-            data: $(this).serialize(),
-            beforeSend: function(){
-                $('form[id^="edit_workstation"] .modal-body').replaceWith(
-                    `<div class="container p-5 text-center">
-                        <div class="spinner-border text-primary" role="status">
-                            <span class="sr-only">Loading...</span>
-                        </div>
-                    </div>`
-                );
-            },
-            success: function () {
-                $('.spinner-border').css('display','none');
-                $('div[aria-labelledby="editWorkStationModalLabel"]').modal('hide');
-                window.location="/workstations";
-            },
-            error: function (xhr) {
-                console.log(xhr, "Erreur lors de la modification");
-            }
-        });
-    });
-
-    $('form[id^="edit_realisation_range"]').submit(function (e) {
-        const newUrl = "rangeRealisation/edit/" + $(this).attr('data-whatever');
-        e.preventDefault();
-        $.ajax({
-            type: $(this).attr('method'),
-            url: newUrl,
-            data: $(this).serialize(),
-            beforeSend: function(){
-                $('form[id^="edit_realisation_range"] .modal-body').replaceWith(
-                    `<div class="container p-5 text-center">
-                        <div class="spinner-border text-primary" role="status">
-                            <span class="sr-only">Loading...</span>
-                        </div>
-                    </div>`
-                );
-
-            },
-            success: function () {
-                $('.spinner-border').css('display','none');
-                $('div[aria-labelledby="editRangeRealisationModalLabel"]').modal('hide');
-                window.location = '/home';
-            },
-            error: function (xhr) {
-                console.log(xhr, "Erreur lors de la modification");
-            }
-        });
-
-        const collapse = document.querySelector('.collapse');
-        collapse.className.replace('collapse','collapse show');
-
-        window.scroll({
-            top: 200,
-            behavior: 'smooth'
-        });
-    });
+    modalGeneric.on('hidden.bs.modal', function (e) {
+        $('#modal-generic-content').html(
+            `<div class="container p-5 text-center">
+               <div class="spinner-border text-primary" role="status">
+                   <span class="sr-only">Loading...</span>
+               </div>
+           </div>`
+        )
+    })
 }
-
 export const ajaxSelectMachine = () => {
 
     const $workStation = $('#operation_workStation');
@@ -183,7 +27,6 @@ export const ajaxSelectMachine = () => {
     operationMachineAdd ? operationMachineAdd.setAttribute('disabled','disabled') : null
     $workStation.change(function() {
         const $form = $(this).closest('form');
-        const time =$('#operation_time');
         let data = {};
         data[$workStation.attr('name')] = $workStation.val();
         const selectWorkStation = document.querySelector("#container_add_operation #operation_workStation")
--- assets/app.js
+++ assets/app.js
@@ -3,15 +3,13 @@ import './styles/app.css';
 // start the Stimulus application
 import './bootstrap';
 import 'bootstrap';
-import { ajaxModalHome,ajaxSelectMachine,ajaxSelectUserAndWorkStation} from './ajax'
+import {AjaxModal, ajaxSelectMachine, ajaxSelectUserAndWorkStation} from './ajax'
 import { handleChangeType } from "./dropdownEvents";
-import $ from "jquery";
-
 
 window.addEventListener('load', () => {
-   ajaxModalHome();
    ajaxSelectMachine();
    ajaxSelectUserAndWorkStation();
    handleChangeType();
+   AjaxModal();
 })
 
